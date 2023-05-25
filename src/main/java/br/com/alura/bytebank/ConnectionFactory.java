2e228af38d371628d92dc618d58251a1b626a607
Vinicius Porto <viniciusportto@outlook.com>
2023-05-24 23:14:37 -0300

    using HIKARI

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,17 +1,29 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
     public Connection recuperarConexao() {
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=998358");
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("998358");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
+    }
+
 }
