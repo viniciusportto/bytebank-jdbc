3e579e5ae775e2edf8dc8ee1ead7c0ab26084c3f
Vinicius Porto <viniciusportto@outlook.com>
2023-05-24 22:20:57 -0300

    preparing to listing count

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -6,36 +6,50 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
     private Connection conn;
 
-    ContaDAO(Connection connection){
+    ContaDAO(Connection connection) {
         this.conn = connection;
     }
 
-    public void salvar(DadosAberturaConta dadosDaConta){
+    public void salvar(DadosAberturaConta dadosDaConta) {
         var cliente = new Cliente(dadosDaConta.dadosCliente());
         var conta = new Conta(dadosDaConta.numero(), cliente);
 
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" + "VALUES (?, ?, ?, ?, ?)";
 
-        try{
+        try {
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
 
             preparedStatement.setInt(1, conta.getNumero());
             preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
-            preparedStatement.setString(3,dadosDaConta.dadosCliente().nome());
-            preparedStatement.setString(4,dadosDaConta.dadosCliente().cpf());
-            preparedStatement.setString(5,dadosDaConta.dadosCliente().email());
+            preparedStatement.setString(3, dadosDaConta.dadosCliente().nome());
+            preparedStatement.setString(4, dadosDaConta.dadosCliente().cpf());
+            preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
 
             preparedStatement.execute();
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
 
     }
 
+    public Set<Conta> listar() {
+        Set<Conta> contas = new HashSet<>();
+
+        String sql = "SELECT * FROM conta";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
